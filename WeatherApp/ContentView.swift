0aba101c33372c33c2f445554f231a1cad578a5e
avinebel <nebel2007@gmail.com>
2024-05-30 17:10:48 -0400

    first attempt at api call but still broken perhaps? we shall see

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        let colors: [Color] = [.red, .blue]
+        TabView {
+            ForEach(colors, id: \.self) { color in
+                VStack {
+                    color
+                }
+            }
         }
-        .padding()
+        .tabViewStyle(.page)
+        .onAppear() {
+            dataService.callAPI(dataService)
+        }
+        
     }
 }
 
