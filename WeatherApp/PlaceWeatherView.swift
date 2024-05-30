3bfd5a340b876a6124eab62f663ea9e67994a80d
Ress1206-bit <adam.ress@icloud.com>
2024-05-30 17:10:56 -0400

    Adding Weather View

---
+++ WeatherApp/PlaceWeatherView.swift
@@ -0,0 +1,18 @@
+//
+//  PlaceWeatherView.swift
+//  WeatherApp
+//
+//  Created by Adam Ress on 5/30/24.
+//
+
+import SwiftUI
+
+struct PlaceWeatherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlaceWeatherView()
+}
